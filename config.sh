f781fb4b878f16d335a60eaf4ac72e909adce90b
Hassan Kibirige <has2k1@gmail.com>
2020-03-19 21:16:33 +0300

    MAINT: Update acc. to MacPython/scipy-wheels

--- config.sh
+++ config.sh
@@ -1,37 +1,31 @@
 # Define custom utilities
 # Test for OSX with [ -n "$IS_OSX" ]
-
-OPENBLAS_VERSION=0.2.18
+# See env_vars.sh for extra environment variables
 source gfortran-install/gfortran_utils.sh
-
-function pre_build {
-    # Any stuff that you need to do before you start building the wheels
-    # Runs in the root directory of this repository.
-    :
-    # Bug Fix
-    # https://github.com/matthew-brett/multibuild/issues/163
-    # https://github.com/SHTOOLS/SHTOOLS/issues/149
-    unset FFLAGS
-}
 
 # From https://github.com/MacPython/scipy-wheels
 function build_wheel {
     if [ -z "$IS_OSX" ]; then
+        unset FFLAGS
+        export LDFLAGS="-shared -Wl,-strip-all"
         build_libs $PLAT
         # build_pip_wheel does not work with versioneer
         # https://github.com/matthew-brett/multibuild/issues/11
         build_pip_wheel $@
     else
+        export FFLAGS="$FFLAGS -fPIC"
         build_osx_wheel $@
     fi
 }
 
 # From https://github.com/MacPython/scipy-wheels
 function build_libs {
-    if [ -n "$IS_OSX" ]; then return; fi  # No OpenBLAS for OSX
     local plat=${1:-$PLAT}
     local tar_path=$(abspath $(get_gf_lib "openblas-${OPENBLAS_VERSION}" "$plat"))
-    (cd / && tar zxf $tar_path)
+    # Sudo needed for macOS
+    local use_sudo=""
+    [ -n "$IS_OSX" ] && use_sudo="sudo"
+    (cd / && $use_sudo tar zxf $tar_path)
 }
 
 # used by build_osx_wheel
@@ -46,29 +40,20 @@ function set_arch {
 }
 
 # From https://github.com/MacPython/scipy-wheels
+function build_wheel_with_patch {
+    # Patch numpy distutils to fix OpenBLAS build
+    (cd .. && ./patch_numpy.sh)
+    bdist_wheel_cmd $@
+}
+
+# From https://github.com/MacPython/scipy-wheels
 function build_osx_wheel {
-    # Build dual arch wheel
-    # Standard gfortran won't build dual arch objects, so we have to build two
-    # wheels, one for 32-bit, one for 64, then fuse them.
+    # Build 64-bit wheel
+    # Standard gfortran won't build dual arch objects.
     local repo_dir=${1:-$REPO_DIR}
-    local wheelhouse=$(abspath ${WHEEL_SDIR:-wheelhouse})
     local py_ld_flags="-Wall -undefined dynamic_lookup -bundle"
-    local wheelhouse32=${wheelhouse}32
 
     install_gfortran
-
-    # 32-bit wheel
-    local arch="-m32"
-    set_arch $arch
-    # Build libraries
-    build_libs i686
-    # Build wheel
-    mkdir -p $wheelhouse32
-    export LDSHARED="$CC $py_ld_flags"
-    export LDFLAGS="$arch $py_ld_flags"
-    build_pip_wheel "$repo_dir"
-    mv ${wheelhouse}/*whl $wheelhouse32
-
     # 64-bit wheel
     local arch="-m64"
     set_arch $arch
@@ -76,12 +61,10 @@ function build_osx_wheel {
     # Build wheel
     export LDSHARED="$CC $py_ld_flags"
     export LDFLAGS="$arch $py_ld_flags"
-    build_pip_wheel "$repo_dir"
-
-    # Fuse into dual arch wheel(s)
-    for whl in ${wheelhouse}/*.whl; do
-        delocate-fuse "$whl" "${wheelhouse32}/$(basename $whl)"
-    done
+    # Work round build dependencies spec in pyproject.toml
+    # See e.g.
+    # https://travis-ci.org/matthew-brett/scipy-wheels/jobs/387794282
+    build_wheel_cmd "build_wheel_with_patch" "$repo_dir"
 }
 
 function run_tests {
@@ -91,8 +74,6 @@ function run_tests {
     test_cmd="import sys, skmisc; sys.exit(skmisc.test())"
 
     if [ -n "$IS_OSX" ]; then
-        # Test both architectures on OSX
-        arch -i386 python -c "$test_cmd"
         arch -x86_64 python -c "$test_cmd"
     else
         python -c "$test_cmd"
---
+++ env_vars.sh
@@ -0,0 +1,6 @@
+# Environment variables for build
+OPENBLAS_VERSION="v0.3.7"
+MACOSX_DEPLOYMENT_TARGET=10.9
+
+# Enable Python fault handler on Pythons >= 3.3.
+PYTHONFAULTHANDLER=1
