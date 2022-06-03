c3d9fd17d08ccdc97f0601daf2fa7e732d24c72d
Hassan Kibirige <has2k1@gmail.com>
2022-06-03 18:27:29 +0300

    Clean up & set the BUILD_COMMIT in separate file

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -8,16 +8,22 @@ TEST_DEPENDS="$NP_TEST_DEP $OTHER_TEST_DEPS"
 SKIP_TESTS=false
 UNAME=$(uname)
 NATIVE_PLAT=$(uname -m)
+BUILD_COMMIT_FILE="${BUILD_DIR}/.github/workflows/build_commit.sh"
 
-# When crosspiling we cannot install the built wheel
+# When crosspiling we cannot install the built wheel, we trust
+# that it works!
 if [[ "$PLAT" != "$NATIVE_PLAT" ]]; then
-  SKIP_TESTS=true
+  if [[ "$PLAT" != "universal2" ]]; then
+    SKIP_TESTS=true
+  fi
 fi
 
 if [[ "$UNAME" == "Linux" ]]; then
   # https://github.com/multi-build/multibuild/issues/470
   ! git config --global --add safe.directory "*"
 fi
+
+if [ -r "$BUILD_COMMIT_FILE" ]; then source "$BUILD_COMMIT_FILE"; fi
 
 echo "::group::Install a virtualenv"
   source multibuild/common_utils.sh
---
+++ .github/workflows/build_commit.sh
@@ -0,0 +1,2 @@
+# scikit-misc commit to built
+BUILD_COMMIT="v0.1.4"
